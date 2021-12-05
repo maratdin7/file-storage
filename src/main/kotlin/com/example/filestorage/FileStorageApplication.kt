174bd0efbfbb704f7c513920f39d98aa9f3689f2
Динмухаметов Марат Линарович <maratdin7@gmail.com>
2021-12-05 19:17:39 +0300

    #3 Spring is enabled and logs are removed from the output

--- src/main/kotlin/com/example/filestorage/FileStorageApplication.kt
+++ src/main/kotlin/com/example/filestorage/FileStorageApplication.kt
@@ -3,10 +3,13 @@ package com.example.filestorage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 import java.io.File
 import java.io.FileOutputStream
 import java.net.URL
+import java.util.*
 
 @SpringBootApplication
 class FileStorageApplication
@@ -39,8 +42,18 @@ class DownloadFile(private val defaultPath: String) {
 }
 
 fun main(args: Array<String>) {
-    val defaultPath = if (args.isNotEmpty()) args[0] else return
+  val defaultPath = Properties().run {
+        val stream = ClassLoader.getSystemResourceAsStream("application.properties")
+        load(stream)
+        getProperty("defaultPath", "/downloads")
+    }
+
     runBlocking<Unit> {
         launch(Dispatchers.IO) { DownloadFile(defaultPath).readFiles() }
+        launch {
+            runApplication<FileStorageApplication>(*args) {
+                setBannerMode(Banner.Mode.OFF)
+            }
+        }
     }
 }
