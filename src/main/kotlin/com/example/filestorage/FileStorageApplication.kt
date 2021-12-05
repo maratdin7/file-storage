5ef91af8e51f2cd22b55cfb071472cad008327f2
Динмухаметов Марат Линарович <maratdin7@gmail.com>
2021-12-05 19:57:41 +0300

    #3 Added the ability to delete all files from the directory

--- src/main/kotlin/com/example/filestorage/FileStorageApplication.kt
+++ src/main/kotlin/com/example/filestorage/FileStorageApplication.kt
@@ -3,19 +3,34 @@ package com.example.filestorage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import org.apache.tomcat.util.http.fileupload.FileUtils
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import java.io.File
 import java.io.FileOutputStream
 import java.net.URL
+import java.nio.file.Paths
 import java.util.*
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
-class FileStorageApplication
+class FileStorageApplication {
+    @Value("\${clearDirectoryAfterStart}")
+    private var isDirectoryNeedClear: Boolean = false
+
+    @Value("\${defaultPath}")
+    private lateinit var defaultPath: String
+    @PostConstruct
+    fun clearDirectory() {
+        if (isDirectoryNeedClear.not()) return
+        val dir = File(defaultPath)
+        FileUtils.cleanDirectory(dir)
+    }
+}
 
 class DownloadFile(private val defaultPath: String) {
-
     private fun download(link: String, path: String) {
         URL(link).openStream().use { input ->
             FileOutputStream(path).use { output ->
